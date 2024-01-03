8c4164eede9975a6f76b5578da6188f50e8706f9
Dipansu <dipansukapil143@gmail.com>
2024-01-03 22:58:08 +0530

    test cases added

---
+++ src/test/java/com/example/ecommers/controllers/ProductControllerMvcTest.java
@@ -0,0 +1,12 @@
+package com.example.ecommers.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import com.example.ecommers.Controller.ProductController;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerMvcTest {
+	
+	
+
+}
--- src/test/java/com/example/ecommers/controllers/ProductcontrollerTest.java
+++ src/test/java/com/example/ecommers/controllers/ProductcontrollerTest.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.example.ecommers.Controller.ProductController;
 import com.example.ecommers.ThirdPartyClient.FakeStoreClient.FakeStoreClient;
+import com.example.ecommers.dtos.GenericProductDto;
 import com.example.ecommers.exception.ProductNotFoundException;
+import com.example.ecommers.service.ProductService;
 
 import jakarta.inject.Inject;
 
@@ -23,6 +28,9 @@ public class ProductcontrollerTest {
 	
 	 @Inject
 	 private FakeStoreClient fakeStoreClient;
+	 
+	 @MockBean
+	  private ProductService productService;
 	
 //	@Test
 //	@DisplayName("Testing 1 plus 2")
@@ -33,10 +41,30 @@ public class ProductcontrollerTest {
 //      //If assert is TRUE, test case succeeds, else fails.
 //	}
 	
+//  @Test
+//  void testGetProductByIdNegativeTC() throws ProductNotFoundException {
+//      assertThrows(ProductNotFoundException.class, () -> productController.getProductById(10000L));
+//      //assertNull(fakeStoreClient.getProductById(1000L));
+//  }
+  
   @Test
-  void testGetProductByIdNegativeTC() throws ProductNotFoundException {
-      //assertThrows(ProductNotFoundException.class, () -> productController.getProductById(10000L));
-      assertNull(fakeStoreClient.getProductById(1000L));
+  void testGetProductByIdMocking() throws ProductNotFoundException {
+	  
+	  GenericProductDto genericProductDto = new GenericProductDto();
+	  
+	  when(productService.getProductById(10L)).thenReturn(genericProductDto);
+	  
+	  GenericProductDto genericProductDto2 = productController.getProductById(10L);
+	  
+	  assertEquals(genericProductDto, genericProductDto2);
+  }
+  
+  @Test
+  void testGetProductByIdMockingException() throws ProductNotFoundException {
+	  
+	  when(productService.getProductById(10L)).thenThrow(ProductNotFoundException.class);
+	  
+	  assertThrows(ProductNotFoundException.class, () -> productService.getProductById(10L));
   }
 	
 
